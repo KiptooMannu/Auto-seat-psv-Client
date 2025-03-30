0c757e6ef33d550b94176d6d5ac90c745d8b1578
Kiptoo Emmanuel <149665572+KiptooMannu@users.noreply.github.com>
2025-03-31 02:49:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Auto-seat-psv-Client/', // Match repository name exactly
+  base: '/Auto-seat-psv-Client/', // Must match your GitHub repo name exactly
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    emptyOutDir: true // Cleans the dist folder before build
   }
 })
