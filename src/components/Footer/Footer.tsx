c499e8a76e73d363f60ec7ab92d08411640249b3
Kiptoo Emmanuel <149665572+KiptooMannu@users.noreply.github.com>
2025-03-31 16:10:51 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 
 const Footer = () => {
   return (
@@ -7,14 +7,7 @@ const Footer = () => {
         <p className="text-sm text-gray-300">
           © {new Date().getFullYear()} Auto Seat PSV. All Rights Reserved.
         </p>
-        <div className="mt-2">
-          <Link to="/dashboard" className="text-blue-400 hover:underline mx-2">
-            Dashboard
-          </Link>
-          <Link to="/contact" className="text-blue-400 hover:underline mx-2">
-            Contact Us
-          </Link>
-        </div>
+        
       </div>
     </div>
   );
