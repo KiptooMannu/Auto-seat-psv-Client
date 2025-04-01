4a7d1f3636034fc7dc3d5bd504b2493a1666310c
Kiptoo Emmanuel <149665572+KiptooMannu@users.noreply.github.com>
2025-04-01 13:36:45 +0300

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,13 @@
-const Footer = () => {
+
+
+export const Footer = () => {
   return (
-    <div className="text-white py-4">
-      <div className="container mx-auto px-4 text-center">
+    <footer className="bg-gray-800 text-white w-full py-4">
+      <div className="mx-auto px-4 text-center">
         <p className="text-sm text-gray-300">
           © {new Date().getFullYear()} Auto Seat PSV. All Rights Reserved.
         </p>
       </div>
-    </div>
+    </footer>
   );
 };
